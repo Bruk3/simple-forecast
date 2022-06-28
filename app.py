21b286477bfea25800dc8d8f0d6a91039525fbb9
Bruk <bbz2103@columbia.edu>
2022-06-28 12:59:56 +0000

    delete unncessay code

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 import os
+
 from flask import jsonify
 
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-QUOTE_SERVICE_URL = os.environ.get("QUOTE_SERVICE_URL")
 
 
 WEATHER = [
     {"Temperature": "40", "Precipitation": "20%", "Wind": "18 mph", "Comment": "Layer Up!"}, 
     {"Temperature": "108", "Precipitation": "2%", "Wind": "8 mph", "Comment": "Sunny day, Beach day!" }
 ]
-
-def fetch_quote():
-    contents = urllib.request.urlopen(
-            "http://{QUOTE_SERVICE_URL}/api/quote".format(
-                QUOTE_SERVICE_URL=QUOTE_SERVICE_URL
-            )
-        ).read()
-    return json.loads(contents)
-
 
 @app.route('/api/today')
 def get_today_forecast():
